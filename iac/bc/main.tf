7cd98ccdf45f5ce71b3ed6b8b4a14b991e85f273
Artem_is_env0 <artem.artyunov@env0.com>
2026-02-19 15:30:19 -0500

    Create main.tf

---
+++ iac/bc/main.tf
@@ -0,0 +1,17 @@
+provider "local" {}
+
+variable "marker_version" {
+  type        = string
+  description = "Bump this to force a plan diff."
+  default     = "1.0.0"
+}
+
+resource "local_file" "bc_marker" {
+  filename = "${path.module}/.env0-demo-${terraform.workspace}-bc.txt"
+  content  = <<EOT
+env0 demo marker
+component=bc
+workspace=${terraform.workspace}
+marker_version=${var.marker_version}
+EOT
+}
