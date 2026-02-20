7f40dd0ca31a14fd435c4c3842f055e8913688dd
Artem_is_env0 <artem.artyunov@env0.com>
2026-02-20 10:54:37 -0500

    Create main.tf

---
+++ iac/bc-cpu/main.tf
@@ -0,0 +1,17 @@
+provider "local" {}
+
+variable "marker_version_for_cpu_AMD_EPYC" {
+  type        = string
+  description = "Bump this to force a plan diff. AMD EPYC CPU DIFF"
+  default     = "1.0.1"
+}
+
+resource "local_file" "api_marker" {
+  filename = "${path.module}/.env0-demo-${terraform.workspace}-epyc-cpu-route.txt"
+  content  = <<EOT
+env0 demo marker
+component=api-route
+workspace=${terraform.workspace}
+marker_version=${var.marker_version}
+EOT
+}
