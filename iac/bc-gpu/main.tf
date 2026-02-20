8c5f65f87e5c4f3591ed12658920247a8b543df2
Artem_is_env0 <artem.artyunov@env0.com>
2026-02-20 14:55:48 -0500

    Update main.tf

--- iac/bc-gpu/main.tf
+++ iac/bc-gpu/main.tf
@@ -1,6 +1,6 @@
 provider "local" {}
 
-variable "marker_version_for_GPU_NVIDIA_RTX6000_GB202-INS-TYPE" {
+variable "marker_version_gpu_NVIDIA_RTX6000_GB202" {
   type        = string
   description = "Bump this to force a plan diff. Nvidia GPU DIFF"
   default     = "1.0.1"
@@ -10,8 +10,8 @@ resource "local_file" "api_marker" {
   filename = "${path.module}/.env0-demo-${terraform.workspace}-epyc-cpu-route.txt"
   content  = <<EOT
 env0 demo marker
-component=api-route
+component=bc-gpu
 workspace=${terraform.workspace}
-marker_version=${var.marker_version}
+marker_version=${var.marker_version_gpu_NVIDIA_RTX6000_GB202}
 EOT
 }
