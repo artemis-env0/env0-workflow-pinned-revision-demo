8a355d7c0942c9fa90e13e795a3b656216af08d6
Artem_is_env0 <artem.artyunov@env0.com>
2026-02-23 17:38:32 -0500

    Add local provider and storage marker variable

--- iac/bc-storage/main.tf
+++ iac/bc-storage/main.tf
@@ -1,1 +1,17 @@
-..
+provider "local" {}
+
+variable "marker_version_NVME_MS_AZURE_STORAGE" {
+  type        = string
+  description = "Bump this to force a plan diff. AZ NVME STORAGE"
+  default     = "1.0.1"
+}
+
+resource "local_file" "api_marker" {
+  filename = "${path.module}/.env0-demo-${terraform.workspace}-storage-az-nvme.txt"
+  content  = <<EOT
+env0 demo marker
+component=bc-storage
+workspace=${terraform.workspace}
+marker_version=${var.marker_version_NVME_MS_AZURE_STORAGE}
+EOT
+}
